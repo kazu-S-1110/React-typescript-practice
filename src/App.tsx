6d82681b44efc954f8f19c58001af1e8739652f1
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-02 08:29:22 +0900

    Hooksなどの型注釈の方法を学習

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState} from 'react'; //ReactHooksなどで型注釈をする場合には基本的に型推論を利用して書いていく。
 import './App.css';
 import Hello from "./components/Hello"
 
 function App() {
+  const [name, setName] = useState<string | null>("Peter");
   return (
     <div>
-      <Hello message="I am Jack."></Hello>
+      <Hello message={`I am ${name}!`}></Hello>
     </div>
   );
 }
+//型注釈で困ったらhttps://github.com/typescript-cheatsheets/reactを見てみる。
 
 export default App;
